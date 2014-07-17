2257973ae7fc5e5bdd2db15160dc475f8134bb15
luult <luulethe@gmail.com>
2014-07-17 14:15:16 +0700

    update model

--- codeweb/src/main/java/com/springapp/mvc/User.java
+++ codeweb/src/main/java/com/springapp/mvc/User.java
@@ -9,13 +9,13 @@ public class User
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Basic
+    @Column (name = "first_name")
     private String firstName;
 
-    @Basic
+    @Column (name = "last_name")
     private String lastName;
 
-    @Basic
+    @Column (name = "email")
     private String email;
 
     public Long getId() {
